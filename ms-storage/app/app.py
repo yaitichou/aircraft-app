8467aef69e60c0e4438224e1de2bb0b4964900fe
yaitichou <yoann.aitichou@orange.fr>
2023-10-03 22:16:43 +0100

    add comments and documentation

--- ms-storage/app/app.py
+++ ms-storage/app/app.py
@@ -12,9 +12,11 @@ app = FastAPI(title="StorageAPI")
 
 @app.post("/store-result")
 async def request_detection(detection: str = Body(...), file: UploadFile = File(...)):
-
+    
+    # Read received data as json
     json_data = json.loads(detection)
 
+    # Store the file on server
     out_file_path = "/ms-storage-data/upload/"+file.filename
     async with aiofiles.open(out_file_path, 'wb') as out_file:
         while content := await file.read(1024):  # async read chunk
@@ -25,6 +27,7 @@ async def request_detection(detection: str = Body(...), file: UploadFile = File(
         "path":out_file_path
         }
     
+    # Save the detection result and the path to the file on elasticsearch
     r = requests.post('http://elasticsearch:9200/detection/_doc/',json=data_to_save)
     
     return r.content
@@ -34,11 +37,12 @@ async def request_detection(detection: str = Body(...), file: UploadFile = File(
 @app.get("/search_all")
 async def request_search_all():
 
+    # Get all detection elements
     r_saved = requests.get('http://elasticsearch:9200/detection/_doc/_search')
 
     json_raw_result_list = json.loads(r_saved.content)["hits"]["hits"]
 
-
+    
     result_list = [raw_result["_source"]["result"] for raw_result in json_raw_result_list]
 
     return {"results":result_list}
